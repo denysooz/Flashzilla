b7cdc1505888bb4b5ad4388abbee62cf9eee2603
denysooz <den.darevskiy@mail.ru>
2024-08-20 21:56:31 -0400

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Denis Dareuskiy on 20.08.24.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
