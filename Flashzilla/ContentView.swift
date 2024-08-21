867815d6d7b2fd801feaf8e1b46fb276564df063
denysooz <den.darevskiy@mail.ru>
2024-08-20 23:19:29 -0400

    Initial commit

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -8,12 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var currentAmount = 0.0
+    @State private var finalAmount = 1.0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, World!")
+                .scaleEffect(finalAmount + currentAmount)
+                .gesture (
+                    MagnifyGesture()
+                        .onChanged { value in
+                            currentAmount = value.magnification - 1
+                        }
+                        .onEnded { value in
+                                finalAmount += currentAmount
+                            currentAmount = 0
+                        }
+                )
         }
         .padding()
     }
