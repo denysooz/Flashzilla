cc27ae8a1d5c45f334d49ca7498c2720be7b93ed
denysooz <den.darevskiy@mail.ru>
2024-08-24 20:29:01 -0500

    Added background image and swipe actions

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -7,27 +7,64 @@
 
 import SwiftUI
 
+extension View {
+    func stacked(at position: Int, in total: Int) -> some View {
+        let offset = Double(total - position)
+        return self.offset(y: offset * 10)
+    }
+}
+
 struct ContentView: View {
-    
-    @State private var currentAmount = 0.0
-    @State private var finalAmount = 1.0
+    @Environment(\.accessibilityDifferentiateWithoutColor) var accessibilityDifferentiateWithoutColor
+    @State private var cards = Array<Card>(repeating: .example, count: 10)
     
     var body: some View {
-        VStack {
-            Text("Hello, World!")
-                .scaleEffect(finalAmount + currentAmount)
-                .gesture (
-                    MagnifyGesture()
-                        .onChanged { value in
-                            currentAmount = value.magnification - 1
+        ZStack {
+            Image(.background)
+                .resizable()
+                .ignoresSafeArea()
+            
+            VStack {
+                ZStack {
+                    ForEach(0..<cards.count, id: \.self) { index in
+                        CardView(card: cards[index]) {
+                            withAnimation {
+                                removeCard(at: index)
+                            }
                         }
-                        .onEnded { value in
-                                finalAmount += currentAmount
-                            currentAmount = 0
-                        }
-                )
+                            .stacked(at: index, in: cards.count)
+                    }
+                }
+            }
+            
+            if accessibilityDifferentiateWithoutColor {
+                VStack {
+                    Spacer()
+                    
+                    HStack {
+                        Image(systemName: "xmark.circle")
+                            .padding()
+                            .background(.black.opacity(0.7))
+                            .clipShape(.circle)
+                        
+                        Spacer()
+                        
+                        Image(systemName: "checkmark.circle")
+                            .padding()
+                            .background(.black.opacity(0.7))
+                            .clipShape(.circle)
+                    }
+                    .foregroundStyle(.white)
+                    .font(.largeTitle)
+                    .padding()
+                }
+            }
         }
-        .padding()
+        
+    }
+    
+    func removeCard(at index: Int) {
+        cards.remove(at: index)
     }
 }
 
