ab682b92bfc359b850991579b375487cc1013414
denysooz <den.darevskiy@mail.ru>
2024-08-24 19:26:47 -0500

    Created card model and card view

---
+++ Flashzilla/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Denis Dareuskiy on 24.08.24.
+//
+
+import Foundation
+
+struct Card {
+    var prompt: String
+    var answer: String
+    
+    static let example = Card(prompt: "1+2=?", answer: "3")
+}
